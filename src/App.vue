0304414f3f377a1d17761f323a66b1ae3f15d9ad
Craig Cronin <craig.cronin@gmail.com>
2018-05-13 22:57:21 -0400

    Add store and auth module

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-container">
+      <center-container>
+      </center-container>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CenterContainer from './components/lib/center-container'
 
 Vue.config.productionTip = false
 
+Vue.component('center-container', CenterContainer)
+
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  template: '<App />',
+  components: { App },
+})
